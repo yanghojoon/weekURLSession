96c19097165c701f3e524e42f94ec231e2e87479
yanghojoon <yanghojoon8487@gmail.com>
2022-01-15 20:39:35 +0900

    feat: 상품 리스트를 가져오는 GET 메서드 구현

--- WeekURLSession/Util/Network.swift
+++ WeekURLSession/Util/Network.swift
@@ -8,5 +8,16 @@
 import Foundation
 
 struct Network {
+    let jsonParsor: JSONParser
     
+    func requestProductsList(pageNumber: Int,
+                             itemsPerPage: Int,
+                             completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) {
+        let url = "https://market-training.yagom-academy.kr/api/products?page-no=\(pageNumber)&items-per-page=\(itemsPerPage)"
+        guard let url = URL(string: url) else { return }
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        let task = URLSession.shared.dataTask(with: request, completionHandler: completionHandler)
+        task.resume()
+    }
 }

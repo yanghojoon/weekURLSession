52e7c45fe71571075517c77376455f934aa96197
yanghojoon <yanghojoon8487@gmail.com>
2022-01-15 19:26:39 +0900

    Initial Commit

---
+++ WeekURLSession/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeekURLSession
+//
+//  Created by 양호준 on 2022/01/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
